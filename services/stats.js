ce7a4a99d78838e0b30c42d6e21eb986969056ff
Gaëtan Jacquet <gja@re-flow.io>
2020-02-17 16:50:40 +0100

    stat by arr and stat by type

--- services/stats.js
+++ services/stats.js
@@ -3,12 +3,70 @@ const indexName = config.get('elasticsearch.index_name');
 
 exports.statsByArrondissement = (client, callback) => {
     // TODO Compter le nombre d'anomalies par arondissement
-    callback([]);
+    client.search({
+        index: indexName,
+        size: 0,
+        body: {
+            aggs: {
+                arrondissements: {
+                    terms: {
+                        field: 'arrondissement.keyword',
+                        size: 20
+                    }
+                }
+            }
+        }
+    })
+    .then(resp => {
+        
+        let res = resp.body.aggregations.arrondissements.buckets.map(elem => {
+            return {
+                arrondissement: elem.key,
+                count: elem.doc_count
+            }
+        })
+        callback(res);
+    })
+    
 }
 
 exports.statsByType = (client, callback) => {
     // TODO Trouver le top 5 des types et sous types d'anomalies
-    callback([]);
+    client.search({
+        index: indexName,
+        size: 0,
+        body: {
+            aggs: {
+                types: {
+                    terms: {
+                        field: 'type.keyword'
+                    },
+                    aggs: {
+                        soustypes: {
+                            terms: {
+                                field: 'sous_type.keyword'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
+    .then(resp => {
+        let res = resp.body.aggregations.types.buckets.map(elem => {
+            return {
+                type: elem.key,
+                count: elem.doc_count,
+                sous_types: elem.soustypes.buckets.map(sstype => {
+                    return {
+                        sous_type: sstype.key,
+                        count: sstype.doc_count
+                    }
+                })
+            }
+        });
+        callback(res);
+    })
 }
 
 exports.statsByMonth = (client, callback) => {
